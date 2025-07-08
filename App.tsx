0ef2ff22f53a2c8c6038a1e2d60295dc277aa027
Arman Ansari <iopmobile@gllfl.com>
2025-07-08 11:30:57 +0530

    audit task progress

--- App.tsx
+++ App.tsx
@@ -1,28 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import AuditFormScreen from './src/screens/AuditFormScreen';
+import AuditSummaryScreen from './src/screens/AuditSummaryScreen';
+import AuditHistoryScreen from './src/screens/AuditHistoryScreen';
+import PolicyViewerScreen from './src/screens/PolicyViewerScreen';
+import { ContextProvider } from './src/RoleProvider/useContextProvider';
+// navigation between use
+const Stack = createNativeStackNavigator();
+//  all screen define
+export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <ContextProvider> 
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="AuditForm" component={AuditFormScreen} />
+          <Stack.Screen name="Summary" component={AuditSummaryScreen} />
+          <Stack.Screen name="History" component={AuditHistoryScreen} />
+          <Stack.Screen name="Policy" component={PolicyViewerScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;

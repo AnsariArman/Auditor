53401b82a2bda4ac3493998b16f0b029d0b25c24
Arman Ansari <iopmobile@gllfl.com>
2025-07-11 12:52:01 +0530

    some chnages for permission camera

--- App.tsx
+++ App.tsx
@@ -7,13 +7,18 @@ import AuditSummaryScreen from './src/screens/AuditSummaryScreen';
 import { ContextProvider } from './src/RoleProvider/useContextProvider';
 import AuditHistoryScreen from './src/screens/AuditHistoryScreen';
 import PolicyViewerScreen from './src/screens/PolicyViewerScreen';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 // navigation between use
 const Stack = createNativeStackNavigator();
 //  all screen define
 export default function App() {
   return (
+    <SafeAreaView style={{flex:1}}>
     <ContextProvider> 
       <NavigationContainer >
+            <StatusBar backgroundColor="#32656A" barStyle="dark-content" />
+
         <Stack.Navigator    screenOptions={{headerShown: false}} initialRouteName='LoginScreen'>
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="AuditFormScreen" component={AuditFormScreen} />
@@ -23,5 +28,6 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </ContextProvider>
+    </SafeAreaView>
   );
 }
